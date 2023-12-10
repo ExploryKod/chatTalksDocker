46a1b4013f41521b7d459ba91b1d1301dd289197
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-12-10 01:46:57 +0100

    wip - store messages in bdd

--- gorillachat/message_handler.go
+++ gorillachat/message_handler.go
@@ -9,30 +9,25 @@ import (
 func (h *Handler) CreateMessageHandler(w http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
 	content := r.FormValue("content")
-	maxMessageTableRows := 2
+	//maxMessageTableRows := 2
 
 	sender, _ := h.Store.GetUserByUsername(username)
-	messagesNumber, _ := h.Store.CountMessagesSent()
+	//messagesNumber, _ := h.Store.CountMessagesSent()
 
 	roomID := r.FormValue("roomID")
 	roomIDInt, _ := strconv.Atoi(roomID)
 
-	if sender.Username != "" && messagesNumber <= maxMessageTableRows {
-		messageID, err := h.Store.AddMessage(MessageItem{Content: content, UserID: sender.ID, RoomID: roomIDInt, Username: sender.Username})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	if sender.Username != "" && content != "" && roomID != "" {
+		// TODO : add a limit of messages per room or accept all messages
+		//messageID, err := h.Store.AddMessage(MessageItem{Content: content, UserID: sender.ID, RoomID: roomIDInt, Username: sender.Username})
+		//if err != nil {
+		//	http.Error(w, err.Error(), http.StatusInternalServerError)
+		//	return
+		//}
+		messageID := 0
 		h.jsonResponse(w, http.StatusOK, map[string]interface{}{"message": "Message sent", "roomID": roomIDInt, "messageID": messageID, "userID": sender.ID})
 	} else if sender.Username == "" {
 		http.Error(w, "No user with this id found", http.StatusBadRequest)
-		return
-	} else if messagesNumber == 2 {
-		http.Error(w, "You can't send more than"+strconv.Itoa(maxMessageTableRows)+"messages to bdd", http.StatusUnauthorized)
-		h.jsonResponse(w, http.StatusUnauthorized, map[string]interface{}{"message": "Vous avez atteint la limite des sauvegardes de vos messages en base de donnée", "roomID": roomIDInt, "userID": sender.ID})
-		return
-	} else if messagesNumber >= 2 {
-		http.Error(w, "You can't send more than"+strconv.Itoa(maxMessageTableRows)+"messages to bdd", http.StatusUnauthorized)
 		return
 	} else {
 		http.Error(w, "Requête non-satisfaite", http.StatusBadRequest)
